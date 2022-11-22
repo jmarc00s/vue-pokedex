e467c6403584aeb3dac868cd03de43cc3bc69ef9
João Marcos <jmarcossferreira@hotmail.com>
2022-11-22 17:46:29 -0300

    Create pokemon search component

---
+++ src/components/PokemonSearch.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useStore } from '@/store';
+import { ref } from 'vue';
+
+const store = useStore();
+
+const search = ref('');
+
+const handleSubmit = () => {
+  store.setFilter(search.value);
+  search.value = '';
+};
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" :on-keydown.enter="handleSubmit">
+    <v-text-field label="Pesquisar" v-model="search" variant="underlined" />
+  </form>
+</template>
