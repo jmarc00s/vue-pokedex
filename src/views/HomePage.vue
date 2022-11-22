f24bb8937ebe5f89be973d14bd5d6f13fa1a24ea
João Marcos <jmarcossferreira@hotmail.com>
2022-11-22 17:51:45 -0300

    Add search on homePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import PokemonList from '@/components/PokemonList.vue';
+import PokemonSearch from '@/components/PokemonSearch.vue';
 </script>
 <template>
   <h1 class="text-center text-h6">Welcome to Vue Pokedex</h1>
+  <PokemonSearch />
   <PokemonList />
 </template>
