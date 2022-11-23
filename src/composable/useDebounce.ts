ce1fecd8d900a27c7973a16ea27d36aada5331d7
João Marcos <jmarcossferreira@hotmail.com>
2022-11-23 18:32:45 -0300

    Improve readbility on useDebounce

--- src/composable/useDebounce.ts
+++ src/composable/useDebounce.ts
@@ -3,12 +3,12 @@ import { ref } from 'vue';
 export function useDebounce() {
   const timeout = ref<any>(undefined);
 
-  function createDebounce(fn: Function, delay: number = 500) {
+  function debounce(fn: Function, delay: number = 500) {
     clearTimeout(timeout.value);
     timeout.value = setTimeout(() => {
       fn();
     }, delay);
   }
 
-  return { debounce: createDebounce };
+  return { debounce };
 }
