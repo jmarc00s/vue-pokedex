052718398b6f41c1a3aacb0d492e545a6ecf5e79
João Marcos <jmarcossferreira@hotmail.com>
2022-11-25 10:22:34 -0300

    Create separate files for plugin config

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,21 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from 'vue-query';
-import type { VueQueryPluginOptions } from 'vue-query';
 
 import App from './App.vue';
 import router from './router';
 
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-
-import axios from 'axios';
-import { i18n } from './i18n';
-
-axios.defaults.baseURL = 'http://localhost:3000';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
-const vueQueryPluginOptions: VueQueryPluginOptions = {
-  queryClientConfig: {
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  },
-};
+import { i18n } from '@/i18n';
+import { vuetifyConfig } from '@/plugins/vuetify';
+import { vueQueryPluginOptions } from '@/plugins/vue-query';
+import '@/plugins/axios';
 
 const app = createApp(App);
 
 app.use(i18n);
 app.use(createPinia());
 app.use(router);
-app.use(vuetify);
+app.use(vuetifyConfig);
 app.use(VueQueryPlugin, vueQueryPluginOptions);
 
 app.mount('#app');
---
+++ src/plugins/vue-query/index.ts
@@ -0,0 +1,11 @@
+import type { VueQueryPluginOptions } from 'vue-query';
+
+export const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+};
